56554a5bd742b7e723c6d8fd2d12ff023c90d1cf
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2024-01-30 17:31:02 +0600

    Установил dayjs и добавил в компонент отображений постов

--- src/MessageList/MessageList.tsx
+++ src/MessageList/MessageList.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import dayjs from 'dayjs';
 
 const MessageList: React.FC<MessageListProps> = ({ messages }) => (
   <div>
     {messages.map((msg) => (
       <div key={msg._id} className='card'>
         <div className='date'>
-          <span className='date-item'>Date: {msg.datetime}</span>
+          <span className='date-item'>Date: {dayjs(msg.datetime).format('MMMM D, YYYY [at] HH:mm:ss A')}</span>
         </div>
         <strong className='author'>Author: {msg.author}</strong> <br/>
         <span className='message'>
